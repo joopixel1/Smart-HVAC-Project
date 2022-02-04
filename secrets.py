405c6f70686717a499b68cb0ff72f69fc8ab91a9
Mat Wymore <mlwymore@gmail.com>
2022-02-03 20:23:18 -0600

    Secrets, secrets, are such fun!

---
+++ secrets.py
@@ -0,0 +1,14 @@
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+
+secrets = {
+    'ssid' : 'IASTATE',
+    'password' : '', # Leave blank for IASTATE
+    'aio_username' : '',
+    'aio_key' : '', # Found on Adafruit IO web portal
+    'timezone' : "America/Chicago", # http://worldtimeapi.org/timezones
+    'broker' : 'io.adafruit.com',
+    'port' : 1883,
+    'primary_node_ip' : '',
+    'secondary_node_ip' : ''
+}
