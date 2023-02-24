78dee727d261fd6a84af2ad35849a2bebb97dffc
Obimakinde <joo@iastate.edu>
2023-02-24 12:55:19 -0600

    lab6

--- zone.py
+++ zone.py
@@ -3,12 +3,7 @@
 class Zone:
     def __init__(self, simul ) -> None:
         self.temp = 20
-<<<<<<< HEAD
         self.damper = 10
-=======
-        self.damper = 1
-        self.delta = 1
->>>>>>> 4aa1a585363510f06df42e9246976d71d1bfc1ba
         self.simul = simul
 
     def getTemp(self):
@@ -19,13 +14,7 @@ class Zone:
         
     def updateTemp(self):
         if(self.simul.heating):
-<<<<<<< HEAD
             self.temp += self.damper*(self.simul.heaterTemp - self.temp)/100
         elif(self.simul.cooling):
             self.temp += self.damper*(self.simul.coolerTemp - self.temp)/100
-=======
-            self.temp += self.damper*self.delta
-        elif(self.simul.cooling):
-            self.temp -= self.damper*self.delta
->>>>>>> 4aa1a585363510f06df42e9246976d71d1bfc1ba
             
