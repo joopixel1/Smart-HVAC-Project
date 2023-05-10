ab82c9def739c455c2f699576b7630d7401d69dd
Obimakinde <joo@iastate.edu>
2023-05-09 19:36:59 -0500

    finger

---
+++ secrets.py
@@ -0,0 +1,14 @@
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+
+secrets = {
+    'ssid' : 'IASTATE',
+    'password' : '', # Leave blank for IASTATE
+    'timezone' : "America/Chicago", # http://worldtimeapi.org/timezones
+    'mqtt_username' : '',
+    'mqtt_key' : '', # Found on Adafruit IO web portal
+    'mqtt_broker' : '10.26.47.68', # hey its me boy write this: hostname -I
+    'port' : 1883,
+    'primary_node_ip' : '10.26.47.50',
+    'secondary_node_ip' : '10.26.45.169'
+}
